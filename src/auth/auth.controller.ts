8a2cd348f558e38ada137755bfd17a587614f8ae
bilalahmad9755 <bilal.ahmed.9755@gmail.com>
2023-08-23 23:04:21 +0500

    added signup,login and user duplication testcases

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,11 +6,14 @@ import {
   HttpStatus,
   ValidationPipe,
   UsePipes,
+  Res
 } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login';
 import { EthereumAddressValidationPipe } from './auth.validation';
 import { AddUserDto } from '../user/dto/add-user';
+
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
@@ -18,14 +21,16 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   @Post('login')
   @UsePipes(new ValidationPipe())
-  login(@Body() loginDto: LoginDto) {
-    return this.authService.signIn(loginDto.username, loginDto.password);
+  login(@Body() loginDto: LoginDto, @Res() res: Response) {
+    this.authService.signIn(loginDto.username, loginDto.password);
+    res.status(HttpStatus.OK).json([{message: "login successful"}]);
   }
 
   @Post('signup')
   @UsePipes(new EthereumAddressValidationPipe())
-  async signUp(@Body() addUserDto: AddUserDto) {
-    return this.authService.signUp(addUserDto);
+  async signUp(@Body() addUserDto: AddUserDto, @Res() res: Response) {
+    await this.authService.signUp(addUserDto);
+    res.status(HttpStatus.CREATED).json([{message: "signin successful"}]);
   }
 
   @Post('signup/verify')
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,31 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { AddUserDto } from './dto/add-user';
+import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
 
 // This should be a real class/interface representing a user entity
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>,) {}
-  async getUsers(): Promise<User[]>{
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  async getUsers(): Promise<User[]> {
     return this.userModel.find().exec();
   }
 
-  async getUserByAddress(walletAddress: string): Promise<User[]>
-  {
-    return this.userModel.find({walletAddress}).exec();
+  async getUserByAddress(walletAddress: string): Promise<User[]> {
+    return this.userModel.find({ walletAddress }).exec();
   }
 
-  async addUser(addUserDto: AddUserDto): Promise<User>
-  {
+  async addUser(addUserDto: AddUserDto) {
+    // check for duplicated ethereum address...
+    const walletAddress = await this.userModel.findOne({
+      walletAddress: addUserDto.walletAddress,
+    }).exec();
+    console.log("duplicated record: ", walletAddress);
+    if (walletAddress !== null) {
+      throw new HttpException('user duplication', 500);
+    }
     const addUser = new this.userModel(addUserDto);
-    return addUser.save();
+    addUser.save();
+    return;
   }
 
-  async getUserByName(name: string):Promise<User[]>
-  {
-    return this.userModel.find({name}).exec();
+  async getUserByName(name: string): Promise<User[]> {
+    return this.userModel.find({ name }).exec();
   }
 }
